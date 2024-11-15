117e5641783e23e5a19841537835a69775f0873c
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-11-15 22:07:24 +0100

    7) Exercise Defensive programming

--- week2.js
+++ week2.js
@@ -63,13 +63,22 @@ console.log('It still works');
 // 7) Exercise Defensive programming
 
 function letterFinder(word, match){
-  var condition1 = typeof word == 'string' && world.length >= 2;
+  var condition1 = typeof word == 'string' && word.length >= 2;
   var condition2 = typeof match == 'string' && match.length == 1;
-  for(var i = 0; i < word.length; i++){
-    if(word[i] == match){
-      console.log('Found the', match, 'at', i);
-    } else {
-      console.log('---No match dound at', i);
+
+  if(condition1 && condition2){
+    for(var i = 0; i < word.length; i++){
+      if(word[i] == match){
+        console.log('Found the', match, 'at', i);
+      } else {
+        console.log('---No match found at', i);
+      }
     }
+  } else {
+    console.log('Please pass correct arguments to the function.');
   }
 }
+
+letterFinder([], []);
+letterFinder('cat', 'c');
+
