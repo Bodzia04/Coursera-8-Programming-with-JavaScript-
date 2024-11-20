a36669c1c94b24f0db05046e6816103b703d9f57
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-11-20 22:54:04 +0100

    aDraft

--- aDraft.js
+++ aDraft.js
@@ -35,3 +35,19 @@ function localVar(){
 }
 
 console.log(value);
+
+const car = {};
+
+car.color = 'red';
+car.turnKey = function(){
+  console.log('engine running');
+}
+
+car.turnKey2 = () => {
+  console.log('engine running 2');
+  return 'engine running 2 return';
+}
+
+console.log(car.color)
+console.log(car.turnKey());
+console.log(car.turnKey2());
