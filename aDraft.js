f1e7c0e79208dc05dab41c718e9ab6a0f3a34d9a
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-11-25 21:14:12 +0100

    aDraft

--- aDraft.js
+++ aDraft.js
@@ -30,24 +30,69 @@ function add(a, b){
 }
 
 
-function localVar(){
-  let value = 1;
-}
+// function localVar(){
+//   let value = 1;
+// }
 
-console.log(value);
+// console.log(value);
 
-const car = {};
+// const car = {};
 
-car.color = 'red';
-car.turnKey = function(){
-  console.log('engine running');
-}
+// car.color = 'red';
+// car.turnKey = function(){
+//   console.log('engine running');
+// }
 
-car.turnKey2 = () => {
-  console.log('engine running 2');
-  return 'engine running 2 return';
-}
+// car.turnKey2 = () => {
+//   console.log('engine running 2');
+//   return 'engine running 2 return';
+// }
 
 console.log(car.color)
 console.log(car.turnKey());
 console.log(car.turnKey2());
+
+function noDefaultParam(number){
+  console.log('Answer:', number * number);
+  
+}
+
+noDefaultParam();
+
+class NoDefaultParam{
+  constructor(number1 = 1, number2 = 4, string){
+    this.number1 = number1;
+    this.number2 = number2;
+    this.string = string;
+  }
+  calculate(){
+    if(this.string){
+      console.log(this.number1 + this.number2, this.string);
+      return;
+    }
+    return 'it is false!'
+  }
+};
+
+let fail = new NoDefaultParam(1, 2, true);
+console.log(fail.calculate());
+
+
+
+class Animal {
+  sayHell(){
+    console.log('hello');
+    
+  }
+}
+
+class Dog extends Animal {
+  sayHell(){
+    console.log('hey');
+    
+  }
+}
+
+let result = new Dog();
+result.sayHell()
+
