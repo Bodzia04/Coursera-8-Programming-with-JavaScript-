14906ca0289241a447ca9c3d0bd305a3883796b4
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2024-12-03 21:58:54 +0100

    4) JavaScript in the Browser​ -> 7) Exercise Capture Data

--- week3.js
+++ week3.js
@@ -137,3 +137,35 @@ function animalCan(){
 };
 
 animalCan();
+
+
+
+
+// 4) JavaScript in the Browser​
+// 7) Exercise Capture Data
+// Task 2: Get h1 into a variable
+const h1 = document.querySelector('h1');
+
+// Task 3: Code an array
+const arr = ['Example Domain', 'First Click', 'Second Click', 'Third Click'];
+
+// Task 4: Write a click-handling function
+function handleClicks(){
+  switch(h1.innerText){
+      case arr[0]:
+          h1.innerText = arr[1];
+          break;
+      case arr[1]:
+          h1.innerText = arr[2];
+          break;
+      case arr[2]:
+          h1.innerText = arr[3];
+          break;
+      default:
+          h1.innerText = arr[0];
+          break;
+  };
+};
+
+// Task 5: Add an event listener
+h1.addEventListener('click', handleClicks);
